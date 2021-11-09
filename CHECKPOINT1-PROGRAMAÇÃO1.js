29a36099b6ee0c66ea6b8c3c665760c734825912
kaiorbm <kakaiorbm@gmail.com>
2021-11-09 20:19:58 -0300

    Programa

---
+++ CHECKPOINT1-PROGRAMAÇÃO1.js
@@ -0,0 +1,44 @@
+const comida = pipoca => {
+  if (pipoca <= 20) {
+    console.log('prato pronto, bom apetite !!!')
+  } else if (pipoca > 20) {
+    console.log('comida queimou')
+  } else if (3 * pipoca) console.log('kabumm')
+}
+comida(20)
+
+const comida2 = macarrão => {
+  if (macarrão <= 16) {
+    console.log('prato pronto, bom apetite !!!')
+  } else if (macarrão > 16) {
+    console.log('comida queimou')
+  } else if (3 * macarrão) console.log('kabumm')
+}
+comida2(16)
+
+const comida3 = carne => {
+  if (carne <= 30) {
+    console.log('prato pronto, bom apetite !!!')
+  } else if (carne > 30) {
+    console.log('comida queimou')
+  } else if (3 * carne) console.log('kabumm')
+}
+comida3(63)
+
+const comida4 = feijão => {
+  if (feijão <= 24) {
+    console.log('prato pronto, bom apetite !!!')
+  } else if (feijão > 24) {
+    console.log('comida queimou')
+  } else if (3 * 24) console.log('kabumm')
+}
+comida4(25)
+
+const comida5 = carne => {
+  if (carne <= 30) {
+    console.log('prato pronto, bom apetite !!!')
+  } else if (carne > 30) {
+    console.log('comida queimou')
+  } else if (3 * carne) console.log('kabumm')
+}
+comida5(20)
